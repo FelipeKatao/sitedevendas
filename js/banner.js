2d33e33291ecc1841343f5676c1c4034961ed8b2
Felipe Catão do Nascimento <SNMACT145@firjan.com.br>
2023-08-30 11:14:30 -0300

    Criacao de banner interativo

--- js/banner.js
+++ js/banner.js
@@ -1,8 +1,12 @@
 
 class Banner{
-    CriarBanner(IdAlvo,Texto){
+    CriarBanner(IdAlvo,ListaBanner){
         let Elemento = document.getElementById(IdAlvo)
-        Elemento.innerHTML +="<article>"+Texto+"</article>"
+        Elemento.innerHTML +="<article></article>"
+    }
+    ModificaBanner(IdAlvo="",ListaBanner=[""],Index=0){
+        let Banner =  document.getElementById(IdAlvo).getElementsByTagName("article")[0]
+        Banner.innerHTML= ListaBanner[Index]
     }
 } 
 
--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ nav.CriarNovoMenu("barra_01",Menus)
 nav.CriarBarraNavUsuario("barra_01","Home")
 
 let B =new Banner();
-B.CriarBanner("banner_1","<h1>Compra rapida</h1> <br/> <h3> Compre roupas mais rapido... </h3>")
+let BannerInfo = ["<h1>Compra rapida</h1> <br/> <h3> Compre roupas mais rapido... </h3>","<h1>Roupas da moda</h1> <br/> <h3> Moda </h3>","<h1> Oferta relampago </h1>"]
+B.CriarBanner("banner_1")
+B.ModificaBanner("banner_1",BannerInfo,2)
+
+let BannerIndex = 0
+setInterval(()=>{
+    //Corpo dela SetInterva
+    if(BannerIndex >= 2){
+        BannerIndex = 0
+    }
+    B.ModificaBanner("banner_1",BannerInfo,BannerIndex)
+    BannerIndex+=1
+},2000)
